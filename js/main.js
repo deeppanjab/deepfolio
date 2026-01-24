f2e8ab8e1fe35d96661ff1405d874589764b6d2f
deeppanjab <jashanghagga84@gmail.com>
2026-01-24 10:13:15 +0530

    Define the testimonials.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+// Swiper Testimonial Cards
+
+const swiper = new Swiper('.testimonials__container', {
+  loop: true,
+  speed: 800,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  slidesPerView: 1,
+  spaceBetween: 30,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+
+});
